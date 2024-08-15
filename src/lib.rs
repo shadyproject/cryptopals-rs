2773b60e3f38bb0a605be0e638c1b71c8ffe4da1
Chris Martin <shady@shadyproject.net>
2024-08-14 20:37:52 -0700

    starting over

--- src/lib.rs
+++
@@ -1,3 +0,0 @@
-pub mod set1;
-pub mod utils;
-pub mod xor;
--- src/main.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    cryptopals_rs::set1::challenge01::run();
-    cryptopals_rs::set1::challenge02::run();
-    cryptopals_rs::set1::challenge03::run();
-    cryptopals_rs::set1::challenge04::run();
-    cryptopals_rs::set1::challenge05::run();
-    cryptopals_rs::set1::challenge06::run();
-}
